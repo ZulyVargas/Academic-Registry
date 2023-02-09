f83dd038d44164b32185f7a0c94e91888e910585
Zuly Vargas <zuly.vargas@mail.escuelaing.edu.co>
2023-02-09 16:08:08 -0500

    first commit - project structure

---
+++ src/main/java/com/perficient/courseregistry/app/CourseRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.perficient.courseregistry.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perficient/courseregistry/app/CourseRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perficient.courseregistry.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
