016fa9856ae4c80017e337896b0d464e7eae445a
Zuly Vargas <zuly.vargas@mail.escuelaing.edu.co>
2023-03-09 10:19:37 -0500

    rename active in subjects table, fix views

--- src/main/resources/static/sql/VIEWS.sql
+++ src/main/resources/static/sql/VIEWS.sql
@@ -2,74 +2,32 @@
 	  ----------  PREREQUISITES_INFO  ----------
 
 create view PREREQUISITES_INFO as
-select b.SUBJECT_ID  as base_id, pr.SUBJECT_ID  as subject_id, pr.title as title, pr.code as code, pr.credits as credits, pr.active as active
+select b.SUBJECT_ID  as base_id, pr.SUBJECT_ID  as subject_id, pr.title as title, pr.code as code, pr.credits as credits, pr.active_subject as active_subject
 	from  prerequisites  p join subjects b on b.subject_id = p.subject_base
 	join subjects pr on pr.subject_id = p.pre_subject;
 
 
---------------- STUDENTS INFO ----------------------
-
-create view INFO_STUDENTS as
-select u.user_id as user_id, u.name as name , u.username as username , u.password as "password"  , u.email as email , u.gender as gender, u.active as active, s.avg, s.status
-from users u join students s on u.USER_ID = s.STUDENT_ID;
-
-select * from INFO_STUDENTS;
-
---------------  INFO PROFESSORS --------------------
+----------------- VIEWS FOR USERS: ------------------------
+	  ----------  INFO_PROFESSORS  ----------
 
 create view INFO_PROFESSORS as
-select u.user_id as user_id, u.name as name , u.username as username , u.password as "password"  , u.email as email , u.gender as gender, u.active as active , p.degree as degree
+select u.user_id as user_id, u.name as name , u.username as username , u.password as "password"  , u.email as email , u.gender as gender, u.active_user as active_user , p.degree as degree
 from users u join professors p on u.user_id = p.professor_id;
 
-select * from INFO_PROFESSORS;
+	  ----------  INFO_STUDENTS   ----------
 
+create view INFO_STUDENTS as
+select u.user_id as user_id, u.name as name , u.username as username , u.password as "password"  , u.email as email , u.gender as gender, u.active_user as active_user, s.avg, s.status
+from users u join students s on u.USER_ID = s.STUDENT_ID;
 
--------------- INFO COURSES: GROUP, SUBJECT, PROFESSOR, YEAR, PERIOD (ALL data from course, subject, professor)------------------------
+----------------- VIEWS FOR COURSES: ------------------------
+
 create view INFO__TOTAL_COURSES as
-select c.course_id  as course_id, c.GROUP_NUMBER  as group_number, c.QUOTA  as quota, c.PROFESSOR_id as professor_id, c.subject_id  as subject, c.status_course as status_course, c.year, c.period, c.active  as activec, 
-u.USER_ID as user_id, u.name as name, u.USERNAME as username, u."password" as password, u.EMAIL as email, u.GENDER as gender, u.ACTIVE as active, 
-s.SUBJECT_ID as SUBJECT_ID, s.title as title, s.CODE as code, s.CREDITS as CREDITS,
-p.degree as degree
+select c.course_id  as course_id, c.GROUP_NUMBER  as group_number, c.QUOTA  as quota, c.PROFESSOR_id as professor_id, c.subject_id  as subject, c.status_course as status_course, c.year, c.period, c.active_course as active_course,
+u.USER_ID as user_id, u.name as name, u.USERNAME as username, u."password" as password, u.EMAIL as email, u.GENDER as gender, u.active_user as active_user, p.degree as degree,
+s.SUBJECT_ID as SUBJECT_ID, s.title as title, s.CODE as code, s.CREDITS as CREDITS, s.active_subject as active_subject
 from subjects s join courses c on c.subject_id = s.subject_id
 				join users u on u.user_id = c.professor_id
 				join professors p on u.user_id  = p.professor_id;
-			
-drop view INFO__TOTAL_COURSES;
-
-SELECT * FROM INFO__TOTAL_COURSES;
-
--------------- INFO COURSES-STUDENTS: GROUP, CODE SUBJECT, STUDENT, EMAIL STUDENT
-
-create view ENROLLED_COURSE  as
-select c.group_number, s.code, u.name as student, u.email 
-from users u join records r on u.user_id = r.student_id
-			 join courses c on c.course_id = r.course_id
-			 join subjects s on c.subject = s.subject_id;
-			
-select * from ENROLLED_COURSE;
-
--------------- GRADES_COURSE ------------
-
-create view GRADES_COURSE  as
-select c.group_number, s.code, u.name as student, u.email, r.grade, r.type 
-from users u join records r on u.user_id = r.student_id
-			 join courses c on c.course_id = r.course_id
-			 join subjects s on c.subject = s.subject_id;
 
 
-select * from GRADES_COURSE;
-
------------- PREREQUISITES_SUBJECT ----------
-
-create view PREREQUISITES_SUBJECT as
-select b.SUBJECT_ID  as subject_id, b.title as subject, pr.SUBJECT_ID as prerrequisite_id, pr.title as prerequisite 
-	from  prerequisites  p join subjects b on b.subject_id = p.subject_base
-	join subjects pr on pr.subject_id = p.pre_subject;
-  
-
-SELECT * FROM PREREQUISITES_SUBJECT;
-
-
-  
-
-SELECT * FROM PREREQUISITES_INFO;
