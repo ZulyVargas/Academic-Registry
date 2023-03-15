8e9487ab77d68eba8fef1b609023c81efda9cacb
Zuly Vargas <zuly.vargas@mail.escuelaing.edu.co>
2023-03-15 10:45:25 -0500

    PUT added

--- src/main/java/com/perficient/courseregistry/app/controller/CourseController.java
+++ src/main/java/com/perficient/courseregistry/app/controller/CourseController.java
@@ -1,21 +1,19 @@
 package com.perficient.courseregistry.app.controller;
 
-
 import com.perficient.courseregistry.app.dto.CourseDTO;
 import com.perficient.courseregistry.app.services.ICourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
-import java.util.Set;
+
 
 @RestController
 @RequestMapping( "/api/v1/courses" )
 public class CourseController {
-
 
     @Autowired
     private final ICourseService courseService;
@@ -25,10 +23,10 @@ public class CourseController {
     }
 
     @GetMapping
-    public ResponseEntity<Set<CourseDTO>> getAllCourses(@RequestParam(name = "limit", defaultValue = "10")  Integer limit,
-                                                        @RequestParam(name = "offset", defaultValue = "0") Integer offset,
-                                                        @RequestParam(name = "active", required = false) Boolean isActive){
-        return new ResponseEntity<Set<CourseDTO>>(courseService.getAllCourses(limit, offset, Optional.ofNullable(isActive)),HttpStatus.OK);
+    public ResponseEntity<List<CourseDTO>> getAllCourses(@RequestParam(name = "limit", defaultValue = "10")  Integer limit,
+                                                         @RequestParam(name = "offset", defaultValue = "0") Integer offset,
+                                                         @RequestParam(name = "active", required = false) Boolean isActive){
+        return new ResponseEntity<List<CourseDTO>>(courseService.getAllCourses(limit, offset, Optional.ofNullable(isActive)),HttpStatus.OK);
     }
 
     @GetMapping(value="/{id}")
@@ -39,7 +37,10 @@ public class CourseController {
     @PostMapping
     public ResponseEntity<CourseDTO> addCourse(@RequestBody @Valid CourseDTO courseDTO){
         return  new ResponseEntity<CourseDTO>(courseService.addCourse(courseDTO), HttpStatus.OK);
-
     }
 
+    @PutMapping
+    public ResponseEntity<CourseDTO> updatedCourse(@RequestBody @Valid CourseDTO courseDTO){
+        return  new ResponseEntity<CourseDTO>(courseService.addCourse(courseDTO), HttpStatus.OK);
+    }
 }
